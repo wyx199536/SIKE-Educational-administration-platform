5937c9f0de04cf31da1b3c81199b99148c9f108d
wyx199536 <935922280@qq.com>
2020-05-06 17:15:36 +0800

    0506-17:15

---
+++ src/components/leftTabbar.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<h1>left-tabbar</h1>	
+	</div>
+</template>
+
+<script>
+	export default{
+		name : 'leftTabbar'
+	}
+</script>
+
+<style>
+</style>
---
+++ src/components/topTabbar.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<h1>top-tabbar</h1>	
+	</div>
+</template>
+
+<script>
+	export default{
+		name : 'topTabbar'
+	}
+</script>
+
+<style>
+</style>
